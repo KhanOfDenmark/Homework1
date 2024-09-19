c0125de0f5598522c76a840db357960a45447f69
KhanOfDenmark <daniilhanin2@mail.ru>
2024-09-19 21:49:55 +0300

    Домашняя работа готова.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args){
-        System.out.println("Моя первая домашняя работа.");
+        System.out.println("Моя первая домашняя работа была выполнена!");
     }
 }
